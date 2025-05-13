391b1398756a06fa6d5e5d146c6bceffa2d25a8f
Nelson <130504241+NelsonLacayo27@users.noreply.github.com>
2025-05-12 21:24:31 -0600

    Update Main.kt
    new pavel

--- TablaMultiplicarAppFixed/TablaMultiplicarAppFixed/src/main/kotlin/com/example/tablamultiplicar/Main.kt
+++ TablaMultiplicarAppFixed/TablaMultiplicarAppFixed/src/main/kotlin/com/example/tablamultiplicar/Main.kt
@@ -9,38 +9,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-/**
- * Descripción:
- *   Crear una aplicación que muestre la tabla de multiplicar del 1 al 12
- *   de un número ingresado por el usuario.
- *
- * Requisitos:
- *   ü Incluir un TextField para ingresar el número base (por ejemplo, 3).
- *   ü Al presionar un Button, mostrar la tabla de multiplicar del número desde 1 hasta 12.
- *   ü Usar una Column para presentar cada línea de la tabla (por ejemplo, “3 x 1 = 3”).
- *   ü Validar que se ingrese un número y manejar posibles errores.
- *   ü Aplicar estilo con MaterialTheme y Modifier.padding para mantener una interfaz ordenada y legible.
- *
- * Estado de la entrega:
- *   Grupo: IICorte-3
- *   Estado de la entrega: No se ha enviado nada en esta tarea
- *   Estado de la calificación: Sin calificar
- *   Tiempo restante: 3 días 4 horas restante
- *   Última modificación: -
- *   Comentarios de la entrega: (0)
- *
- * Criterios de calificación:
- *   1. Funcionamiento general de la aplicación (6 pts)
- *      – La aplicación compila sin errores y realiza correctamente las funciones requeridas.
- *   2. Uso correcto de controles básicos (8 pts)
- *      – Incluye y utiliza adecuadamente TextField, Text, Button.
- *   3. Manejo del estado con Compose (6 pts)
- *      – Uso correcto de remember, mutableStateOf y actualización de interfaz según el estado.
- *   4. Aplicación de Material UI y diseño visual (6 pts)
- *      – Uso apropiado de MaterialTheme, disposición visual clara, espaciado, y adaptación a pantalla.
- *   5. Documentación y limpieza del código (4 pts)
- *      – Código legible, organizado y comentado adecuadamente. Buenas prácticas de programación.
- */
 
 @Composable
 @Preview
